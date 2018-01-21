ce5616a7bad84859cbfaa4fe8b83895f77c784ca
barbaradimitrova <barbara.dimitrova@goeuro.com>
2018-01-21 18:36:26 +0100

    added threads for each incoming transaction

--- src/main/java/com/n26/challenge/transactionapi/service/TransactionService.java
+++ src/main/java/com/n26/challenge/transactionapi/service/TransactionService.java
@@ -3,10 +3,34 @@ package com.n26.challenge.transactionapi.service;
 
 import com.n26.challenge.transactionapi.model.Transaction;
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class TransactionService {
 
+    private final Queue<Transaction> transQueue;
+
+
+    public TransactionService() {
+        this.transQueue = new ConcurrentLinkedQueue<>();
+        Thread thread = new Thread(){
+            public void run() {
+                process();
+            };
+        };
+        thread.start();
+    }
 
     public void addTransaction(Transaction transaction){
+        this.transQueue.add(transaction);
+    }
 
+
+    private void process() {
+        while (true) {
+            Transaction transaction = transQueue.poll();
+            //process transaction
+
+        }
     }
 }
