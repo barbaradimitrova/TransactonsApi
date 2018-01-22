2216b82fccf0799b64b1632589c8cd9ed8a46bdb
barbaradimitrova <barbara.dimitrova@goeuro.com>
2018-01-22 14:58:47 +0100

    added configuration for beans

---
+++ src/main/java/com/n26/challenge/transactionapi/Util/TransactionConfiguration.java
@@ -0,0 +1,22 @@
+package com.n26.challenge.transactionapi.Util;
+
+import com.n26.challenge.transactionapi.endpoints.StatisticEndPoint;
+import com.n26.challenge.transactionapi.endpoints.TransactionEndPoint;
+import com.n26.challenge.transactionapi.service.StatisticsManager;
+import com.n26.challenge.transactionapi.service.TransactionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class TransactionConfiguration {
+
+    @Bean
+    public TransactionManager transactionManager(StatisticsManager statisticsManager) {
+        return new TransactionManager(statisticsManager);
+    }
+    @Bean
+    public StatisticsManager statisticsManager() {
+        return new StatisticsManager();
+    }
+}
